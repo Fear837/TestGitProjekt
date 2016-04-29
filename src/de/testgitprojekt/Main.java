32eba10fdbabeb53e68b68c24c2cbdf1de8684ee
Fear837 <Fear837@web.de>
2016-04-29 13:16:50 +0200

    +Initial commit

---
+++ src/de/testgitprojekt/Main.java
@@ -0,0 +1,17 @@
+package de.testgitprojekt;
+
+/**
+ * TODO: Documentation
+ *
+ * @author Kevin
+ * @since 29.04.2016
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt!");
+	}
+	
+	
+	
+}
